f8a1b2febb786a273921508c1ab5d56dc934a0ff
Muhammet Türkan <83230346+MuhammetTurkan@users.noreply.github.com>
2022-03-27 22:47:45 +0300

    fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,11 +5,7 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-knobs'],
   presets: [path.resolve(__dirname, './next-preset.js')],
   core: {
     builder: 'webpack5'
